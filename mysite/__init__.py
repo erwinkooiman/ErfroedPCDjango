fe361bf57aa7a38e5b3b18638eef765376c7a04e
erwinkooiman <erwinkooiman98@gmail.com>
2021-11-26 13:09:02 +0100

    commit master

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -1,4 +1,1 @@
 default_app_config = 'myapp.apps.MyAppConfig'
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'django_celery_beat',
     'crispy_forms',
 ]
 
